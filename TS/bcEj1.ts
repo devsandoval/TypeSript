eaaa462378a28a29a4b8174ceafb736c6a09f19c
Cesar Sandoval <cesarsandoval.dev@gmail.com>
2024-05-16 23:11:12 -0400

    add ejercicio en clase N1 porcientos (actualizado)

--- TS/bcEj1.ts
+++ TS/bcEj1.ts
@@ -1,9 +1,9 @@
-// ejercicio 1
-//Asigne a la variable m el valor de la expresión: 2 + 7 elevado a 3
-let m: number = (2 + (7**3));
+/* ejercicio 1
+Asigne a la variable m el valor de la expresión: 2 + 7 elevado a 3*/
+let m: number = 2 + (7**3);
 console.log(m);
 
-//Defina la variable n con un valor numérico a su elección
+/*Defina la variable n con un valor numérico a su elección*/
 let n :number = 20;
 console.log(n);
 
@@ -15,22 +15,23 @@ console.log(`El resultado de m + n es: ${m+n}`);
 let Control1: number = 5.5;
 let Control2: number = 4.1;
 let Control3: number = 6.4;
-let total: number = Control1 + Control2 + Control3;
-let Promedio = (Control1 + Control2 + Control3)/total;
-console.log(Promedio);
+let total: number = (Control1 + Control2 + Control3)
+let Promedio: number = (Control1 + Control2 + Control3)/total;
+console.log(`El promedio es ${Promedio}`);
 let mascota: string = 'perritos';
 let cantidad: number = 10;
-let mensaje: string = "Yo tenia " + cantidad + " "+ mascota;
+let mensaje: string = `Yo tenia ${cantidad} ${mascota}`
+console.log(mensaje)
 
 // ejercicio 3
 /*Conversor de divisas
 El dolar hoy esta a 787.65 pesos
-Quiere convertir 10000 peso
+Quiere convertir 10000 pesos
 Le corresponden: 12.695994413762458 dolares  resultado*/
 let dolar: number = 787.65;
 let pesos: number = 10000;
 let  dolaresObtenidos: number = pesos/dolar;
-console.log("La cantidad de dolares obtenidos es: $" + dolaresObtenidos);
+console.log(`La cantidad de dolares obtenidos es: $${dolaresObtenidos}`);
 
 // ejercico 4
 /*. Suponga que estamos generando una calculadora para calcular medidas geométricas con el siguiente
