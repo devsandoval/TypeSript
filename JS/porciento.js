2481aad771ddd9a615532a20180c642c1b6ed725
Cesar Sandoval <cesarsandoval.dev@gmail.com>
2024-06-03 15:32:06 -0400

    se crea el modulo 2

--- JS/porciento.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-let v1 = 60;
-let v2 = 80;
-let suma = v1 + v2;
-let por1 = (v1 * 100) / suma;
-let por2 = (v2 * 100) / suma;
-console.log(`El porsentaje de v1 es ${por1}`);
-console.log(`El porsentaje de v2 es ${por2}`);
--- TS/porciento.ts
+++
@@ -1,9 +0,0 @@
-let v1 : number= 60
-let v2 : number= 80
-let suma : number  = v1+v2
-let por1 : number = (v1*100)/suma
-let por2 : number = (v2*100)/suma
-
-console.log(`El porsentaje de v1 es ${por1}`)
-console.log(`El porsentaje de v2 es ${por2}`)
-
